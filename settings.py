b8b749ff4047de1f9f6386f2414ef665751bb492
saumishr <saumishr@www.domain1.com>
2013-09-01 18:17:46 +0530

    display popup view for facebook login

--- settings.py
+++ settings.py
@@ -407,7 +407,7 @@ SOCIAL_AUTH_DEFAULT_USERNAME =  'new_social_auth_user' # you'll need to import s
 SOCIAL_AUTH_EXTRA_DATA = False
 SOCIAL_AUTH_CHANGE_SIGNAL_ONLY = True
 SOCIAL_AUTH_ASSOCIATE_BY_MAIL = True
-FACEBOOK_AUTH_EXTRA_ARGUMENTS = {'display': 'popup'}
+#FACEBOOK_AUTH_EXTRA_ARGUMENTS = {'display': 'popup'}
 
 TWITTER_EXTRA_DATA = [('profile_image_url', 'profile_image_url')]
 
